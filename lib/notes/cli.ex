99e771b27234608c3d070548322cc2833d6111df
Theo Pack <tf.pack@googlemail.com>
2014-04-28 16:49:38 +0200

    add parser for args and help text

--- lib/notes/cli.ex
+++ lib/notes/cli.ex
@@ -1,5 +1,27 @@
 defmodule Notes.CLI do
+
+	@default_count 5
+
 	def main(args) do
-    	IO.puts "Hello World"
+    	args
+    		|> parse_args 
+    		|> process
+  end
+
+  def parse_args(argv) do
+  	parse = OptionParser.parse(argv, switches: [help: :boolean, priority: :integer],
+  		aliases: [h: :help, p: :priority])
+  	case parse do
+  		{[help: true], _, _} -> :help
   	end
+  end
+
+  def process(:help) do
+		IO.puts """
+		usage: 
+		notes add <note> [--priority n]
+		notes list [count | #{@default_count}]
+		"""
+		System.halt(0)
+	end
 end
