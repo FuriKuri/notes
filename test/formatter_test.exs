e99ff0fd05931cdfc5b2a5339c1c5481ff676c94
Theo Pack <tf.pack@gmail.com>
2014-04-30 18:49:36 +0200

    fix test

--- test/formatter_test.exs
+++ test/formatter_test.exs
@@ -3,26 +3,38 @@ defmodule FormatterTest do
 
   alias Notes.Formatter, as: NF
 
+  test "width of ids where header is max" do
+    entries = [{5, 1, "Two"}, {6, 2, "One"}, {7, 3, "Two"}]
+    width = NF.width_of_ids(entries)
+    assert width == 2
+  end
+
+  test "width of ids" do
+    entries = [{1111, 1, "Five"}, {2222, 2, "One"}, {333, 3, "Very long"}]
+    width = NF.width_of_ids(entries)
+    assert width == 4
+  end
+
   test "width of notes where header is max" do
-    entries = [{1, "Two"}, {2, "One"}, {3, "Two"}]
+    entries = [{5, 1, "Two"}, {6, 2, "One"}, {7, 3, "Two"}]
     width = NF.width_of_notes(entries)
     assert width == 4
   end
 
   test "width of notes" do
-    entries = [{1, "Five"}, {2, "One"}, {3, "Very long"}]
+    entries = [{1, 1, "Five"}, {2, 2, "One"}, {3, 3, "Very long"}]
     width = NF.width_of_notes(entries)
     assert width == 9
   end
 
   test "width of priorities where header is max" do
-    entries = [{1, "Five"}, {222, "One"}, {33, "Very long"}]
+    entries = [{1, 1, "Five"}, {1, 222, "One"}, {1, 33, "Very long"}]
     width = NF.width_of_priorities(entries)
     assert width == 4
   end
 
   test "width of priorities" do
-    entries = [{1, "Five"}, {22222, "One"}, {33, "Very long"}]
+    entries = [{1, 1, "Five"}, {1, 22222, "One"}, {1, 33, "Very long"}]
     width = NF.width_of_priorities(entries)
     assert width == 5
   end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -5,23 +5,24 @@ defmodule ParserNotesTest do
 
 
   test "map priority string to integer type" do
-    assert NP.notes(["1;Note"], 1) == [{1, "Note"}]
+    assert NP.notes(["1;1;Note"], 1) == [{1, 1, "Note"}]
   end
 
   test "map entry to a tuple" do
-    assert NP.notes(["1;Note"], 1) == [{1, "Note"}]
+    assert NP.notes(["1;1;Note"], 1) == [{1, 1, "Note"}]
   end
 
   test "split notes into own elements" do
-    assert NP.notes(["2;Top", "1;Note"], 2) == [{2, "Top"}, {1, "Note"}]
+    assert NP.notes(["1;2;Top", "2;1;Note"], 2) == [{1, 2, "Top"}, {2, 1, "Note"}]
   end
 
   test "sort notes by priority" do
-    assert NP.notes(["2;Medium", "1;Low", "3;High"], 3) == [{3, "High"}, {2, "Medium"}, {1, "Low"}]
+    assert NP.notes(["5;2;Medium", "6;1;Low", "7;3;High"], 3)
+        == [{7, 3, "High"}, {5, 2, "Medium"}, {6, 1, "Low"}]
   end
 
   test "limit numbers of notes" do
-    assert NP.notes(["2;Medium", "1;Low", "3;High"], 2) == [{3, "High"}, {2, "Medium"}]
-    assert NP.notes(["2;Medium", "1;Low", "3;High"], 1) == [{3, "High"}]
+    assert NP.notes(["1;2;Medium", "2;1;Low", "3;3;High"], 2) == [{3, 3, "High"}, {1, 2, "Medium"}]
+    assert NP.notes(["1;2;Medium", "2;1;Low", "3;3;High"], 1) == [{3, 3, "High"}]
   end
 end
