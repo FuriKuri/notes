62352b95bd72df495e6a7054844d664fd66cf7f2
Theo Pack <tf.pack@gmail.com>
2014-04-30 18:36:26 +0200

    replace tabs with spaces

--- lib/notes/cli.ex
+++ lib/notes/cli.ex
@@ -4,17 +4,17 @@ defmodule Notes.CLI do
   alias Notes.Config, as: Config
 
   def main(args) do
-    	args
-    		|> parse_args
-    		|> process
+    args
+      |> parse_args
+      |> process
   end
 
   def parse_args(argv) do
-  	parse = OptionParser.parse(argv,
+    parse = OptionParser.parse(argv,
       switches: [help: :boolean, priority: :integer],
-  		aliases: [h: :help, p: :priority])
-  	case parse do
-  		{[help: true], _, _} -> :help
+      aliases: [h: :help, p: :priority])
+    case parse do
+      {[help: true], _, _} -> :help
 
       {[priority: priority], ["add", note], _} -> {:add, note, priority}
       {_, ["add", note], _} -> {:add, note, 0}
@@ -23,7 +23,7 @@ defmodule Notes.CLI do
       {_, ["ls"], _} -> {:ls, Config.default_count}
 
       {_, _, _} -> :help
-  	end
+    end
   end
 
   def process(:help) do
